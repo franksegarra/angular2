6fdaa1ecd996beedeb37ff0403f91d816c407a8b
segaf <segaf@CRP-IT-PF00W8DJ>
2017-02-02 20:49:44 -0500

    Added ProfileName to player table

--- hsPlayerProfile/Database/Database/player.sql
+++ hsPlayerProfile/Database/Database/player.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[player]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [lastName] VARCHAR(50) NULL, 
-    [firstName] VARCHAR(50) NULL, 
-    [highSchool] INT NULL 
+    [lastName] VARCHAR(50) NOT NULL, 
+    [firstName] VARCHAR(50) NOT NULL, 
+    [highSchool] INT NOT NULL, 
+    [profileName] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [CK_profileName] unique ([profileName]) 
 )
